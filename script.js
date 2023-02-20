5f3894deaf8bde4a5d283301b4d2b740aac3f974
kudravaja <113890695+kudravaja@users.noreply.github.com>
2023-02-20 21:33:51 +0300

    :)

---
+++ script.js
@@ -0,0 +1,24 @@
+// <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+// <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+// <script type="text/javascript">
+//
+// let x;
+// $(window).on('scroll', function() {
+//   x = $(window).scrollTop();
+//   $('.t').css('background-size', 100 + parseInt(x / 20) + 'vw');
+// });
+//
+//
+// </script>
+
+// function setup() {
+//   createCanvas(200, 400);
+//   background(102);
+// }
+//
+// function draw() {
+//   stroke(255);
+//   if (mouseIsPressed === true) {
+//   line(mouseX, mouseY, pmouseX, pmouseY);
+//   }
+// }
