0a82c2a6a39e6fd35e3c47c539ef59a7c425e989
kudravaja <113890695+kudravaja@users.noreply.github.com>
2023-02-27 14:27:18 +0300

    new_version

--- sketch.js
+++ sketch.js
@@ -1,28 +1,3 @@
-// function setup() {
-//   createCanvas(1000, 1000);
-// }
-//
-// function draw() {
-//   if (mouseIsPressed) {
-//     fill(0);
-//   } else {
-//     fill(255);
-//   }
-//   ellipse(mouseX, mouseY, 50, 50);
-// }
-  // const img = document.querySelector('img');
-  //
-  // window.addEventListener('scroll', () => {
-  //   const current = window.scrollTop;
-  //   img.css.clipPath = 'circle(${current + 10}vw at center)';
-  // });
-  //
-  // window.addEventListener('scroll', () => {
-  //   const current = window.scrollTop();
-  //   img.css.clipPath = ('circle', 100 +  parseInt(x / 20) + 'vw');
-  // });
-  //
-
   // section1
   let x;
   $(window).on('scroll', function() {
@@ -47,54 +22,140 @@
     $('.tsecond2').css('background-size', 45 + parseInt(z1 / 0.5) + 'vw');
   });
 
+  // let myP = document.getElementById("myP").className
+
   window.onscroll = function() {myFunction()};
   function myFunction() {
-    if (document.documentElement.scrollTop <0.8*window.innerHeight) {
-      document.getElementById("myP").className = "ttop";
-    }
-    else if (document.documentElement.scrollTop <2.9*window.innerHeight) {
-      document.getElementById("myP").className = "t";
-    }
-    else if (document.documentElement.scrollTop <3.2*window.innerHeight) {
-      document.getElementById("myP").className = "tsecond";
-    }
-    else if (document.documentElement.scrollTop <3.75*window.innerHeight) {
-      document.getElementById("myP").className = "tsecond2";
-    }
-    else {
-      document.getElementById("myP").classList.remove('tsecond2');
-    }
+    // if (document.documentElement.scrollTop <0.8*window.innerHeight) {
+    //   document.getElementById("myP").className = "ttop","ttoptext";
+    // }
+    // else if (document.documentElement.scrollTop <2.9*window.innerHeight) {
+    //   document.getElementById("myP").className = "t";
+    // }
+    // else if (document.documentElement.scrollTop <3.2*window.innerHeight) {
+    //   document.getElementById("myP").className = "tsecond";
+    // }
+    // else if (document.documentElement.scrollTop <3.25*window.innerHeight) {
+    //   document.getElementById("myP").className = "tsecond2";
+    // }
+    // else if (document.documentElement.scrollTop >3.3*window.innerHeight) {
+    //   document.getElementById("myP").classList.remove("tsecond2","tsecond","t","ttop");
+    // }
+
+    // if (document.documentElement.scrollTop >0.01*window.innerHeight) {
+    //   document.getElementById("image1").className = "image1";
+    // }
+
+    // else if (document.documentElement.scrollTop <3.7*window.innerHeight) {
+    //   document.getElementById("image2").className = "image2";
+    // }
+    // else if (document.documentElement.scrollTop <3.9*window.innerHeight) {
+    //   document.getElementById("image3").className = "image3";
+    // }
   }
+
+
+    // else if (document.documentElement.scrollTop <6*window.innerHeight) {
+    //   document.getElementById("image1").classList.remove("image1");
+    // }
+  // }
+
+
+
 
 // section2
-  // let image1;
-  // $(window).on('scroll', function() {
-  //   image1 = $(window).scrollTop();
-  //   $('.image1').css('transform', 100 + parseInt(image1 / 10) + 'vw');
-  // });
+
+ // const scroller = document.getElementById("two");
+
+ $(window).on("scroll", () => {
+    let image0 = $(window).scrollTop();
+    let height = parseInt($(window).height()) * 3.285;
+    let image1 = parseInt(image0) / 30;
+    console.log(image0, height, image1);
+    if (image0 > height) {
+    // if ($(".two").scrollTop() > 1*window.innerHeight){
+      let blur = parseInt(image1) * (0.001) +'vw'
+      let x = parseInt(image1) * (-0.03) + 'vw'
+      let y = parseInt(image1) * (0.06) + 'vw'
+      let z = parseInt(image1) * (0.03) + 'vw'
+      let scale = parseInt(image1/ 100) * (0.4)
+      $('.image1').css('transform', `translate3d(${x},${y},${z})`, `scale3d(${scale},${scale},${scale})`);
+      $('.image1').css('filter', `blur(${blur})`);
+    } else {
+      console.log('dont work')
+    }
+
+      // let image2 = $(window).scrollTop();
+      // let blur2 = parseInt(image2) * (0.001) +'vw'
+      // let x2 = parseInt(image2) * (0.1) + 'vw'
+      // let y2 = parseInt(image2) * (0.2) + 'vw'
+      // let z2 = parseInt(image2) * (0.1) + 'vw'
+      // let scale2 = parseInt(image2/ 100) * (0.4)
+      // $('.image2').css('transform', `translate3d(${x2},${y2},${z2})`, `scale3d(${scale2},${scale2},${scale2})`);
+      // $('.image2').css('filter', `blur(${blur2})`);
+      //
+      // let image3 = $(window).scrollTop();
+      // let blur3 = parseInt(image3) * (0.001) +'vw'
+      // let x3 = parseInt(image3) * (0.15) + 'vw'
+      // let y3 = parseInt(image3) * (-0.05) + 'vw'
+      // let z3 = parseInt(image3) * (0.1) + 'vw'
+      // let scale3 = parseInt(image3/  100) * (0.4)
+      // $('.image3').css('transform', `translate3d(${x3},${y3},${z3})`, `scale3d(${scale3},${scale3},${scale3})`);
+      // $('.image3').css('filter', `blur(${blur3})`);
+
+    // }
+
+
+    // else if ($(".two").scrollTop() < 1*window.innerHeight){
+    //   let blur = parseInt(image1) * (0.001) +'vw'
+    //   let x = parseInt(image1)*(0.001) + 'vw'
+    //   let y = parseInt(image1)*(0.001)  + 'vw'
+    //   let z = parseInt(image1)*(0.001)  + 'vw'
+    //   $('.image1').css('transform', `translate3d(${x},${y},${z})`);
+    //   $('.image1').css('filter', `blur(${blur})`);
+    // }
+});
 
 
 
-  document.onscroll = function() {MyFunction()};
-  function MyFunction() {
-    if (document.documentElement.scrollTop <3.20*window.innerHeight) {
-      document.getElementById("image").className = "image1";
-    }
-    else if (document.documentElement.scrollTop <5.5*window.innerHeight) {
-      document.getElementById("image").className = "image1hover";
-    }
-    else {
-      document.getElementById("image").classList.remove('image1hover');
-    }
-  }
-  // function translateFunction() {
-  // document.getElementById(".image1").css.transform="translateX(100px)"};
-  //
-  // let z3 = ["6","-16","13","-20","-23","9"];
-  // $(window).on('scroll', function() {
-  //   z1 = $(window).scrollTop();
-  //   $('.tsecond2').css.transform = `translate(${z3[1]}%,${forarray[1]}%)`;
-  // });
+
+
+
+
+// const scroller = document.getElementById("scroller");
+// const output = document.getElementById("output");
+//
+// scroller.addEventListener("scroll", () => {
+//  output.textContent = `scrollTop: ${scroller.scrollTop}`;
+// });
+
+ // document.getElementsByClassName("two").onscroll = function() {MyFunction()};
+ //  function MyFunction() {
+ //    if (document.documentElement.scrollTop > 2.0*window.innerHeight) {
+ //      document.getElementById("image1").className = "image1";
+ //    }
+ //  }
+
+//  $(function(){
+//   $('.image').animatescroll({
+//     onScrollStart: function(){
+//        let image1 = $(window).scrollTop();
+//        let blur = parseInt(image1)*0.0001 + 'vw'
+//        let x = parseInt(image1)*(-0.02) + 'vw'
+//        let y = parseInt(image1)*(-0.01)  + 'vw'
+//        let z = parseInt(image1)*(-0.01)  + 'vw'
+//        $('.image1').css('transform', `translate3d(${x},${y},${z})`);
+//        $('.image1').css('filter', `blur(${blur})`);
+//     },
+//     onScrollEnd: function(){
+//       // do something
+//     }
+//   });
+// });
+
+
+
+  // if ($(window).scrollTop() >= window.innerHeight * 0.03);
 
   // let z2;
   // $(window).on('scroll', function() {
@@ -114,3 +175,28 @@
   // transform : 'translate3d(0px, 0px, 0px)'
   // .css({"transform" : "translate3d(0px, + st  + "%")",
   // ({backgroundPosition: '0px'+st+'px'})
+
+  // function setup() {
+  //   createCanvas(1000, 1000);
+  // }
+  //
+  // function draw() {
+  //   if (mouseIsPressed) {
+  //     fill(0);
+  //   } else {
+  //     fill(255);
+  //   }
+  //   ellipse(mouseX, mouseY, 50, 50);
+  // }
+    // const img = document.querySelector('img');
+    //
+    // window.addEventListener('scroll', () => {
+    //   const current = window.scrollTop;
+    //   img.css.clipPath = 'circle(${current + 10}vw at center)';
+    // });
+    //
+    // window.addEventListener('scroll', () => {
+    //   const current = window.scrollTop();
+    //   img.css.clipPath = ('circle', 100 +  parseInt(x / 20) + 'vw');
+    // });
+    //
